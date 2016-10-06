c466990485b11d6816d119b333ae0f309e7fd090
Bert Cotton <bert.cotton@gmail.com>
2016-10-06 06:20:33 -0600

    Added archive.ps1

---
+++ archive.ps1
@@ -0,0 +1,16 @@
+$build = (Get-Content .\build.json | Out-String | ConvertFrom-Json)
+if ($build.deploys)
+{
+  Add-Type -Assembly System.IO.Compression.FileSystem
+  $compressionLevel = [System.IO.Compression.CompressionLevel]::Optimal
+  $build.deploys | ForEach {
+    $directory = $Build.ArtifactStagingDirectory + "/" + $_.name;
+    $output = $Build.BinariesDirectory + "/" + $_.name + ".zip"
+    [System.IO.Compression.ZipFile]::CreateFromDirectory($directory, $output, $compressionLevel, $false)
+    Write-Host "Created Zip $output"
+  }
+}
+else
+{
+  Write-Host "No deploy packages to archive."
+}
