f9721e5cc72d6e5017474720f55b68ff11da182d
Bert Cotton <bert.cotton@gmail.com>
2016-10-06 06:52:23 -0600

    fixing path slash

--- archive.ps1
+++ archive.ps1
@@ -4,8 +4,8 @@ if ($build.deploys)
   Add-Type -Assembly System.IO.Compression.FileSystem
   $compressionLevel = [System.IO.Compression.CompressionLevel]::Optimal
   $build.deploys | ForEach {
-    $directory = $Build.ArtifactStagingDirectory + "/" + $_.name;
-    $output = $Build.ArtifactStagingDirectory + "/artifacts/" + $_.name + ".zip"
+    $directory = $Build.ArtifactStagingDirectory + "\" + $_.name;
+    $output = $Build.ArtifactStagingDirectory + "\artifacts\" + $_.name + ".zip"
     [System.IO.Compression.ZipFile]::CreateFromDirectory($directory, $output, $compressionLevel, $false)
     Write-Host "Created Zip $output"
   }
